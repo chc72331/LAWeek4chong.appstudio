476f9a7092a84f61c7c13e97cde9e151e89d0035
chc72331 <chc72331@creighton.edu>
2021-02-24 14:54:00 -0600

    Create pets.js

---
+++ forms/pets/pets.js
@@ -0,0 +1,19 @@
+// a, b, c
+
+let petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
+
+// d
+let withSal = petNames
+
+// e
+withSal.push('Sal')
+//console.log(withSal)
+
+// f
+console.log(`The third pet is named ${withSal[2]}.`)
+
+// g
+console.log(`The array has ${withSal.length} names in it.`)
+
+// h 
+withSal.pop()
